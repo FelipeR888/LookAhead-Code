c03309bffe3965e09e2572b13d845ff29b7d0538
FelipeR888 <s6fereck@uni-bonn.de>
2020-12-20 15:00:03 +0100

    add batch scripts

---
+++ BatchpyABC/submit_worker.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+HOST_IP=${1}
+PORT=${2}
+TIME=${3}
+CPUSPERTASK=${4}
+cur_PWD=${5}
+
+cd ${cur_PWD}
+
+echo '#### This is the worker script job ####'
+
+# Source module
+source ./load_module.sh
+
+# Define associative array with port and password
+declare -A PW_DICT
+
+# Start redis-worker
+/p/home/jusers/reck1/juwels/keksenv/bin/abc-redis-worker --host=${HOST_IP} --port ${PORT} --runtime ${TIME:0:2}h --processes ${CPUSPERTASK}
